f28a534ee3ea13e83cb7b393f6e0c2be7980affb
Eduardo Nascimento Alves <eduardonasm10@gmail.com>
2026-08-25 17:19:52 -0300

    inicia-exemplo-put

--- src/app/feats/exemplo-put/exemplo-put.ts
+++ src/app/feats/exemplo-put/exemplo-put.ts
@@ -1,9 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ExemploPostService } from '../exemplo-post/exemplo-post-service';
+import { InterfacePut } from './interface-put';
+import { form, required, FormField } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-exemplo-put',
-  imports: [],
+  imports: [FormField],
   templateUrl: './exemplo-put.html',
   styleUrl: './exemplo-put.css',
 })
-export class ExemploPut {}
+export class ExemploPut {
+
+  protected readonly exemploPostService = inject(ExemploPostService);
+
+  protected putModel = signal<InterfacePut>({
+    id: null,
+    userId: null,
+    title: '',
+    body: ''
+  });
+
+  protected usuarios = signal<InterfacePut[]>([]);
+
+  protected putForm = form(this.putModel, (user) => {
+
+    // ID de usuario
+    required(user.id, { message: 'ID é obrigatório' });
+    
+    // UserID de usuario
+    required(user.userId, { message: 'ID é obrigatório' });
+
+    // Title
+    required(user.title, { message: 'Title é obrigatório' });
+
+    // Body
+    required(user.body, { message: 'Body é obrigatório' });
+
+  });
+
+  protected cadastroUsuario(event: SubmitEvent) {
+    event.preventDefault();
+
+    const post = this.putModel();
+
+    this.exemploPostService.cadastrarPostDoService(post).subscribe({
+
+      next: () => {
+        alert ('Post cadastrado!');
+
+        this.putModel.set({
+          id: null,
+          userId: null,
+          title: '',
+          body: ''
+        });
+
+        this.putForm().reset();
+      },
+       
+      error: () => {
+
+        alert ('Algo deu errado!');
+      }
+    });
+
+    this.usuarios.update(valor => [...valor, post]);
+
+  };
+}
