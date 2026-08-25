12a053494198ccf5d2b06273402cf3085e464ece
Eduardo Nascimento Alves <eduardonasm10@gmail.com>
2026-08-25 16:47:00 -0300

    cria-a-requisicao-post-no-componente-exemplo-post-e-adiciona-comentarios

--- src/app/feats/exemplo-post/exemplo-post-service.ts
+++ src/app/feats/exemplo-post/exemplo-post-service.ts
@@ -1,4 +1,19 @@
-import { Service } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Service } from '@angular/core';
+import { InterfacePost } from './interface-post';
 
 @Service()
-export class ExemploPostService {}
+export class ExemploPostService {
+
+    // Injeta HttpClient
+    private readonly httpClient = inject(HttpClient);
+
+    // Cria uma variável que recebe o endereço (URL) da API
+    private readonly urlApi = 'https://jsonplaceholder.typicode.com/posts';
+
+    // Post da API
+    cadastrarPostDoService(postCadastrado: InterfacePost) {
+        return this.httpClient.post(this.urlApi, postCadastrado);
+    };
+
+} 
--- src/app/feats/exemplo-post/exemplo-post.ts
+++ src/app/feats/exemplo-post/exemplo-post.ts
@@ -11,16 +11,20 @@ import { ExemploPostService } from './exemplo-post-service';
 })
 export class ExemploPost {
 
+  // Injeta o serviço "ExemploPostService" no componente (TypeScript)
   protected readonly exemploPostService = inject(ExemploPostService);
 
+  // Inicia o formulário com as informações contidas aqui. Além disso, é para onde as informações vão depois do cadastro 
   protected postModel = signal<InterfacePost>({
     userId: null,
     title: '',
     body: ''
   });
 
+  // Cria a array "usuários" que pega os objetos da interface "InterfacePost" e cria uma array com os valores digitados pelo usuário no formulário no HTML
   usuarios = signal<InterfacePost[]>([]);
 
+  // Passsa o valor do "postModel" para o "postForm", e cria as requisições do formulário
   protected postForm = form(this.postModel, (user) => {
 
     // ID de usuario
@@ -34,20 +38,43 @@ export class ExemploPost {
 
   });
 
+  // Função que é acionada ao clicar o botão "Cadastrar" no HTML
   protected cadastroUsuario(event: SubmitEvent) {
     event.preventDefault();
 
-    const usuario = this.postModel();
+    // Cria uma constante que recebe os valores do "postModel"
+    const post = this.postModel();
 
-    this.postModel.set({
-      userId: null,
-      title: '',
-      body: ''
+    this.exemploPostService.cadastrarPostDoService(post).subscribe({
+
+      // Se o post é enviado corretamente, ele segue a sequência do "next"
+      next: () => {
+
+        // Alerta acionado quando o Post é cadastrado
+        alert ('Post cadastrado!');
+
+        // Limpa o formulário depois de clicar no botão, fazendo com que os valores fiquem nulos
+        this.postModel.set({
+          userId: null,
+          title: '',
+          body: ''
+        });
+
+        // Reseta o formulário
+        this.postForm().reset();
+      },
+       
+      // Se no envio do post acontece um problema e torna inválido a requisição, ela segue a sequência do "error"
+      error: () => {
+
+        // Alerta acionado quando a requisição é inválida
+        alert ('Algo deu errado!');
+      }
     });
 
-    this.usuarios.update(valor => [...valor, usuario]);
+    //Atualiza a lista de usuários mantendo os existentes e adicionando o novo usuário
+    this.usuarios.update(valor => [...valor, post]);
 
-    this.postForm().reset();
   };
 
 }
